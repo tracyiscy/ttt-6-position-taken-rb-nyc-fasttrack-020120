344e69d6349c1d4c1e77b44c1942742506fc3f9a
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-02-01 20:34:10 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board,index)
+  if board[index.to_i] == (" ") || board[index.to_i] == ('') || board[index.to_i] == (nil)
+    return false
+  elsif board[index.to_i] == ("X") || board[index.to_i] == ("O")
+    return true
+  end
+end
