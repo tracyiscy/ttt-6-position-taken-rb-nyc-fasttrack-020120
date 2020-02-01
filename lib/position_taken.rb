4c71962c3b607b38d597ca32d31300df469a4c12
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-02-01 20:33:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
-  if board[index.to_i] == (" ") || ('') || (nil)
+  if board[index.to_i] == (" ") || board[index.to_i] == ('') || board[index.to_i] == (nil)
     return false
   elsif board[index.to_i] == ("X") || ("O")
     return true
